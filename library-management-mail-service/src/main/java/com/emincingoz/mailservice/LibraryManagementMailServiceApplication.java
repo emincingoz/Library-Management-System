4e5ce07b8e100848dedd0b3c2bde8c5b0e46ef3b
Emin Cingöz <emincingoz37@gmail.com>
2022-09-05 09:39:25 +0300

    rabbit mq configuration

---
+++ library-management-mail-service/src/main/java/com/emincingoz/mailservice/LibraryManagementMailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.emincingoz.mailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementMailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementMailServiceApplication.class, args);
+	}
+
+}
